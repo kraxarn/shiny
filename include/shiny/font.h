657d9d834da4858b67771be5fdf45c9c571cfc2e
kraxarn <kraxie@protonmail.com>
2025-11-26 15:45:16 +0100

    This might be better?

--- include/shiny/font.h
+++ include/shiny/font.h
@@ -1,20 +1,12 @@
 #pragma once
 
-#include "shiny/color.h"
-
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_stdinc.h>
 
 typedef struct shiny_font_t shiny_font_t;
 
-shiny_font_t *shiny_font_create(SDL_Renderer *renderer, const Uint8 *data);
-
-bool shiny_font_bake(shiny_font_t *font);
+shiny_font_t *shiny_font_create(SDL_Renderer *renderer, const Uint8 *data, float font_size);
 
 void shiny_font_destroy(shiny_font_t *font);
 
-void shiny_font_set_size(shiny_font_t *font, float size);
-
-void shiny_font_set_color(shiny_font_t *font, shiny_color_t color);
-
-bool shiny_font_draw_text(shiny_font_t *font, float x, float y, const char *text, Sint32 length);
+bool shiny_font_draw_text(const shiny_font_t *font, float x, float y, const char *text, Sint32 length);
--- src/font.c
+++ src/font.c
@@ -1,5 +1,6 @@
 #include "shiny/font.h"
 #include "shiny/color.h"
+#include "shiny/internal/color.h"
 #include "shiny/internal/logcategory.h"
 
 #define STB_RECT_PACK_IMPLEMENTATION
@@ -9,7 +10,6 @@
 #include "stb_truetype.h"
 
 #include <SDL3/SDL_error.h>
-#include <SDL3/SDL_log.h>
 #include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_properties.h>
 #include <SDL3/SDL_rect.h>
@@ -29,19 +29,20 @@ typedef struct shiny_font_t
 	const Uint8 *data;
 	SDL_Renderer *renderer;
 
-	SDL_Surface *glyphs;
+	SDL_Surface *glyphs_surface;
 	SDL_Texture *glyphs_texture;
 
-	stbtt_packedchar packed_chars[ascii_size];
-	stbtt_aligned_quad aligned_quads[ascii_size];
+	SDL_Color color;
+
+	stbtt_bakedchar baked_chars[ascii_size];
+	stbtt_fontinfo info;
 
 	float size;
-
-	// SDL_Palette *palette;
-	// SDL_Texture *texture;
-	// stbtt_bakedchar chars[ascii_size];
-	// float font_size;
-	// shiny_color_t color;
+	float scale;
+	int ascent;
+	int descent;
+	int line_gap;
+	float baseline;
 } shiny_font_t;
 
 Sint64 shiny_font_default_texture_size(SDL_Renderer *renderer)
@@ -55,7 +56,24 @@ Sint64 shiny_font_default_texture_size(SDL_Renderer *renderer)
 		: default_texture_size;
 }
 
-shiny_font_t *shiny_font_create(SDL_Renderer *renderer, const Uint8 *data)
+static bool shiny_font_parse(shiny_font_t *font)
+{
+	if (!stbtt_InitFont(&font->info, font->data, 0))
+	{
+		return SDL_SetError("Font initialisation error");
+	}
+
+	font->scale = stbtt_ScaleForPixelHeight(&font->info, font->size);
+	stbtt_GetFontVMetrics(&font->info, &font->ascent, &font->descent, &font->line_gap);
+	font->baseline = (float) font->ascent * font->scale;
+
+	return true;
+}
+
+// TODO: Move around functions instead
+bool shiny_font_bake(shiny_font_t *font);
+
+shiny_font_t *shiny_font_create(SDL_Renderer *renderer, const Uint8 *data, const float font_size)
 {
 	shiny_font_t *font = SDL_calloc(1, sizeof(shiny_font_t));
 	if (font == nullptr)
@@ -65,36 +83,48 @@ shiny_font_t *shiny_font_create(SDL_Renderer *renderer, const Uint8 *data)
 
 	font->renderer = renderer;
 	font->data = data;
+	font->size = font_size;
+	font->color = shiny_sdl_theme_color(SHINY_COLOR_FOREGROUND);
+
+	if (!shiny_font_parse(font) || !shiny_font_bake(font))
+	{
+		SDL_free(font);
+		return nullptr;
+	}
 
 	return font;
 }
 
-static void shiny_font_pack(shiny_font_t *font)
+static bool shiny_build_palette(SDL_Surface *surface, const SDL_Color color)
 {
-	stbtt_pack_context ctx;
+	constexpr auto color_count = 256; // INDEX8
 
-	constexpr auto stride_in_bytes = 0;
-	constexpr auto padding = 1;
-
-	stbtt_PackBegin(&ctx, font->glyphs->pixels, font->glyphs->w,
-		font->glyphs->h, stride_in_bytes, padding, nullptr);
+	SDL_Palette *palette = SDL_CreateSurfacePalette(surface);
+	if (palette == nullptr)
 	{
-		constexpr int first_codepoint = ascii_begin;
-		constexpr int num_chars = ascii_size;
-
-		stbtt_PackFontRange(&ctx, font->data, 0, font->size,
-			first_codepoint, num_chars, font->packed_chars);
+		return false;
 	}
-	stbtt_PackEnd(&ctx);
 
-	for (auto i = 0; i < ascii_size; i++)
+	SDL_Color colors[color_count];
+	for (auto i = 0; i < color_count; i++)
 	{
-		auto temp_x = 0.F;
-		auto temp_y = 0.F;
-
-		stbtt_GetPackedQuad(font->packed_chars, font->glyphs->w, font->glyphs->h, i,
-			&temp_x, &temp_y, font->aligned_quads + i, 0);
+		colors[i].r = color.r;
+		colors[i].g = color.g;
+		colors[i].b = color.b;
+		colors[i].a = 0;
 	}
+
+	if (!SDL_SetPaletteColors(palette, colors, 0, color_count))
+	{
+		return false;
+	}
+
+	if (!SDL_SetSurfacePalette(surface, palette)) // TODO: Shouldn't be needed
+	{
+		return false;
+	}
+
+	return true;
 }
 
 bool shiny_font_bake(shiny_font_t *font)
@@ -104,100 +134,74 @@ bool shiny_font_bake(shiny_font_t *font)
 		SDL_DestroyTexture(font->glyphs_texture);
 	}
 
-	if (font->glyphs != nullptr)
+	if (font->glyphs_surface != nullptr)
 	{
-		SDL_DestroySurface(font->glyphs);
+		SDL_DestroySurface(font->glyphs_surface);
 	}
 
-	// if (font->font_size <= 0.F)
-	// {
-	// 	return SDL_SetError("Invalid font size: %f", font->font_size);
-	// }
-
-	font->glyphs = SDL_CreateSurface(atlas_width, atlas_height,
-		SDL_PIXELFORMAT_INDEX8);
-	if (font->glyphs == nullptr)
+	font->glyphs_surface = SDL_CreateSurface(atlas_width, atlas_height, SDL_PIXELFORMAT_INDEX8);
+	if (font->glyphs_surface == nullptr)
 	{
 		return false;
 	}
 
-	shiny_font_pack(font);
+	shiny_build_palette(font->glyphs_surface, font->color);
 
-	// TODO
-	SDL_Color colors[256];
-	for (auto i = 0; i < 256; i++)
+	stbtt_BakeFontBitmap(font->data, 0, font->size, font->glyphs_surface->pixels,
+		font->glyphs_surface->w, font->glyphs_surface->h,
+		ascii_begin, ascii_size, font->baked_chars
+	);
+
+	for (auto i = 0; i < ascii_size; i++) // TODO: For vertical alignment, but is it needed?
 	{
-		colors[i].r = i;
-		colors[i].g = i;
-		colors[i].b = i;
+		if (font->baked_chars[i].yoff > font->baseline)
+		{
+			font->baseline = font->baked_chars[i].yoff;
+		}
 	}
-	SDL_Palette *palette = SDL_CreateSurfacePalette(font->glyphs);
-	SDL_SetPaletteColors(palette, colors, 0, 256);
-	SDL_SetSurfacePalette(font->glyphs, palette);
 
-	// const int font_offset = stbtt_GetFontOffsetForIndex(font->font_data, 0);
-	// stbtt_BakeFontBitmap(
-	// 	font->font_data, font_offset, font->font_size,
-	// 	font->glyphs->pixels, font->glyphs->w, font->glyphs->h,
-	// 	ascii_begin, ascii_size,
-	// 	font->chars
-	// );
-	//
-	// font->texture = SDL_CreateTextureFromSurface(font->renderer, font->glyphs);
-	// return font->texture != nullptr;
+	// TODO: Should be safe to destroy surface after creating the texture
+	font->glyphs_texture = SDL_CreateTextureFromSurface(font->renderer, font->glyphs_surface);
+	return font->glyphs_texture != nullptr;
+}
+
+bool shiny_font_draw_text(const shiny_font_t *font, const float x,
+	const float y, const char *text, const Sint32 length)
+{
+	float curr_x = x;
+
+	for (auto i = 0; i < length; i++)
+	{
+		const char chr = text[i];
+		const stbtt_bakedchar *baked_char = font->baked_chars + chr - ascii_begin;
+
+		const SDL_FRect src = {
+			.x = (float) baked_char->x0,
+			.y = (float) baked_char->y0,
+			.w = (float) (baked_char->x1 - baked_char->x0),
+			.h = (float) (baked_char->y1 - baked_char->y0),
+		};
+		const SDL_FRect dst = {
+			.x = curr_x + baked_char->xoff,
+			.y = y + baked_char->yoff,
+			.w = src.w,
+			.h = src.h,
+		};
+
+		if (!SDL_RenderTexture(font->renderer, font->glyphs_texture, &src, &dst))
+		{
+			return false;
+		}
+
+		curr_x += baked_char->xadvance;
+	}
 
 	return true;
 }
 
-bool shiny_font_draw_text(shiny_font_t *font, const float x,
-	const float y, const char *text, const Sint32 length)
-{
-	auto result = true;
-
-	auto x_offset = 0.F;
-	auto y_offset = 0.F;
-
-	for (auto i = 0; i < length; i++)
-	{
-		const int char_index = text[i] - ascii_begin;
-		const stbtt_packedchar *packed_char = &font->packed_chars[char_index];
-		const stbtt_aligned_quad *aligned_quad = &font->aligned_quads[char_index];
-
-		const float width = aligned_quad->x1 - aligned_quad->x0;
-		const float height = aligned_quad->y1 - aligned_quad->y0;
-
-		SDL_FRect src = {
-			.x = aligned_quad->s0 * (float) font->glyphs->w,
-			.y = aligned_quad->t0 * (float) font->glyphs->h,
-			.w = width,
-			.h = height,
-		};
-		SDL_FRect dst = {
-			.x = x + aligned_quad->x0,
-			.y = font->size + y + aligned_quad->y0,
-			.w = width,
-			.h = height,
-		};
-
-		result |= SDL_RenderTexture(font->renderer, font->glyphs_texture, &src, &dst);
-	}
-
-	return result;
-}
-
 void shiny_font_destroy(shiny_font_t *font)
 {
-	SDL_DestroySurface(font->glyphs);
+	SDL_DestroySurface(font->glyphs_surface);
 	SDL_DestroyTexture(font->glyphs_texture);
 	SDL_free(font);
-}
-
-void shiny_font_set_size(shiny_font_t *font, const float size)
-{
-	font->size = size;
-}
-
-void shiny_font_set_color(shiny_font_t *font, const shiny_color_t color)
-{
-	// font->color = color;
 }
