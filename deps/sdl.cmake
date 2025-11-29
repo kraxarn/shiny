537aba0cded99e7fb1ab0827b98c6da5f4c06cc3
kraxarn <kraxie@protonmail.com>
2025-11-29 17:46:10 +0100

    Remove SDL_ttf and freetype dependencies

--- deps/sdl.cmake
+++ deps/sdl.cmake
@@ -5,15 +5,9 @@ FetchContent_Declare(sdl
 	GIT_TAG release-3.2.26
 )
 
-FetchContent_Declare(sdl_ttf
-	GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf.git
-	GIT_TAG release-3.2.2
-)
-
 find_package(SDL3 QUIET)
-find_package(SDL3_ttf QUIET)
 
-if (SDL3_FOUND AND SDL3_ttf_FOUND)
+if (SDL3_FOUND)
 	message(STATUS "Using system SDL")
 else ()
 	message(STATUS "Downloading SDL")
@@ -29,10 +23,6 @@ else ()
 	set(SDL_SENSOR OFF)
 	set(SDL_VIDEO ON)
 	FetchContent_MakeAvailable(sdl)
-	FetchContent_MakeAvailable(sdl_ttf)
 endif ()
 
-target_link_libraries(${PROJECT_NAME} PRIVATE
-	SDL3::SDL3
-	SDL3_ttf::SDL3_ttf
-)
+target_link_libraries(${PROJECT_NAME} PRIVATE SDL3::SDL3)
--- src/clay.c
+++ src/clay.c
@@ -1,5 +1,6 @@
 #define CLAY_IMPLEMENTATION
 #include "clay.h"
 
+#define SDL3_CLAY_NO_SDL_TTF
 #define SDL3_CLAY_IMPL
 #include "renderers/SDL3/clay_renderer_SDL3.h"
