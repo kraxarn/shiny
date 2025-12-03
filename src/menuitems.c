edec707a2ac07d83a0397d07fef9527ea1fff755
kraxarn <kraxie@protonmail.com>
2025-12-03 19:34:45 +0100

    Add menu items element

---
+++ src/menuitems.c
@@ -0,0 +1,29 @@
+#include "shiny/menuitems.h"
+#include "shiny/theme.h"
+#include "shiny/themekey.h"
+#include "shiny/internal/color.h"
+#include "shiny/internal/element.h"
+
+#include "clay.h"
+
+void shiny_menu_items_begin(Clay_Context *context)
+{
+	shiny_element_open(context, nullptr);
+
+	const Clay_ElementDeclaration element = {
+		.layout = (Clay_LayoutConfig){
+			.layoutDirection = CLAY_TOP_TO_BOTTOM,
+			.sizing = (Clay_Sizing){
+				.width = CLAY_SIZING_FIXED(200),
+			},
+		},
+		.cornerRadius = CLAY_CORNER_RADIUS(shiny_theme_corner_radius(SHINY_CORNER_RADIUS_DEFAULT)),
+		.backgroundColor = shiny_clay_theme_color(SHINY_COLOR_WINDOW_BACKGROUND),
+	};
+	shiny_element_configure(&element);
+}
+
+void shiny_menu_items_end()
+{
+	shiny_element_close();
+}
