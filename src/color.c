2645a989ef90315cb34a69322d41801355871951
kraxarn <kraxie@protonmail.com>
2025-12-03 17:57:02 +0100

    Make static to avoid name conflicts

--- src/color.c
+++ src/color.c
@@ -6,22 +6,22 @@
 #include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_stdinc.h>
 
-Uint8 red(const shiny_color_t color)
+static Uint8 red(const shiny_color_t color)
 {
 	return (Uint8) (color >> 16);
 }
 
-Uint8 green(const shiny_color_t color)
+static Uint8 green(const shiny_color_t color)
 {
 	return (Uint8) (color >> 8);
 }
 
-Uint8 blue(const shiny_color_t color)
+static Uint8 blue(const shiny_color_t color)
 {
 	return (Uint8) color;
 }
 
-Clay_Color shiny_clay_color(const shiny_color_t color)
+static Clay_Color shiny_clay_color(const shiny_color_t color)
 {
 	return (Clay_Color){
 		.r = red(color),
@@ -36,7 +36,7 @@ Clay_Color shiny_clay_theme_color(const shiny_theme_color_t color)
 	return shiny_clay_color(shiny_theme_color(color));
 }
 
-SDL_Color shiny_sdl_color(shiny_color_t color)
+static SDL_Color shiny_sdl_color(shiny_color_t color)
 {
 	return (SDL_Color){
 		.r = red(color),
